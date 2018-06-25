dbf15dbf61bbb0016910b0c8b1bfa0a84d227f84
Fenz <uukooo@126.com>
2018-06-25 22:56:27 +0800

    mink2D demo

--- demos/euc2D.js
+++ demos/euc2D.js
@@ -6,7 +6,7 @@ canvas.view( [ -16, 16, ], [ -16, 16, ], 512, );
 
 const p_O= canvas.point( 0, 0, { label:'O', }, );
 const p_A= canvas.point( 1, 1, { label:'A', free:true, }, );
-const p_B= canvas.point( 1, 3, { label:'B', free:true, }, );
+const p_B= canvas.point( 3, 3, { label:'B', free:true, }, );
 
 const l_1= p_A.lineTo( p_B, { label:'l1', hidden:true, }, );
 const O_0= p_O.circle( 8, );
--- demos/mink2D.js
+++ demos/mink2D.js
@@ -5,8 +5,8 @@ const canvas= new Mink2D();
 canvas.view( [ -16, 16, ], [ '-16i', '16i', ], 512, );
 
 const p_O= canvas.point( 0, 0, { label:'O', }, );
-const p_A= canvas.point( 1, 'i', { label:'A', free:true, }, );
-const p_B= canvas.point( 3, '3i', { label:'B', free:true, }, );
+const p_A= canvas.point( -4, 0, { label:'A', free:true, }, );
+const p_B= canvas.point( 3, '2i', { label:'B', free:true, }, );
 
 const l_1= p_A.lineTo( p_B, { label:'l1', hidden:true, }, );
 const O_0= p_O.circle( 0, );
@@ -25,14 +25,15 @@ const O_5i= p_O.circle( '10i', );
 const O_6i= p_O.circle( '12i', );
 const O_7i= p_O.circle( '14i', );
 
-// const foot= l_1.foot( p_O, );
+const foot= l_1.foot( p_O, );
 
-// const c_1= l_1.cross( O_0, );
+const c_1= l_1.cross( O_5, );
 
 canvas.show(
 	l_1,
 	O_0, O_1, O_2, O_3, O_4, O_5, O_6, O_7, O_1i, O_2i, O_3i, O_4i, O_5i, O_6i, O_7i,
 	p_O, p_A, p_B,
+	foot, c_1,
 );
 
 export default canvas;
