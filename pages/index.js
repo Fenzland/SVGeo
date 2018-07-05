6651902c5f4f47d7d9224501b1d4b3f08daef365
Fenz <uukoo@163.com>
2018-07-05 08:41:56 +0800

    add introductions

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,34 @@ export default new Page( {
 			),
 			main(
 				article(
+					p(
+						'This is a library for drawing interactive geometry graphs with ECMAScript work on browsers. ',
+						'SVGeo is powered by ',
+						a( 'SVG', { href:'https://www.w3.org/Graphics/SVG/', target:'_blank', }, ),
+						' and ',
+						a( 'OvO', { href:'https://github.com/Fenzland/ovo.js', target:'_blank', }, ),
+						'. So your browsers must support SVG and ES6 modules. ',
+						'SVGeo is open-sourced licensed under the ',
+						a( 'MIT license', { href:'https://opensource.org/licenses/MIT', target:'_blank', }, ),
+						'. You can access the source codes on the ',
+						a( 'GitHub', { href:'https://github.com/Fenzland/SVGeo', target:'_blank', }, ),
+						'. ',
+					),
+					p(
+						'Currently, we support Euclidean 2D space and Minkowski 2D space. ',
+						'You can draw points, lines and circles in this two type of spaces. ',
+						'Here are what you can do: Drawing a point with (x,y) or (t,x) coordinate; ',
+						'Setting it as free point so that your visitors can drag it with their mouses; ',
+						'Making a animation with setting coordinate of points; ',
+						'Drawing a line with two points. ',
+						'Drawing a circle with a center point and a radius; ',
+						'Crossing two lines, two circles, or a line and a circle, and draw the intersection points; ',
+						'Through a point and drawing a perpendicular line or parallel line of a particular line; ',
+						'Coloring and styling elements .',
+					),
+					p(
+						'Here are demos. ',
+					),
 					div(
 						{ class:'flexible-line', },
 						figure(
