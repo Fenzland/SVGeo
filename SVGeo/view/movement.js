a3a2154c957f7e3b41908be35a4a7aef42451dc1
Fenz <uukooo@126.com>
2018-07-05 22:06:51 +0800

    improve moving, follow mouse when page scroll

--- SVGeo/view/movement.js
+++ SVGeo/view/movement.js
@@ -7,23 +7,35 @@ export default function movement( target, viewport, )
 		if( e.target!==this )
 			return;
 		
-		let [ oX, oY, ]= [ e.offsetX, e.offsetY, ];
+		let [ cX, cY, ]= [ e.clientX, e.clientY, ];
+		let [ sX, sY, ]= [ document.scrollingElement.scrollLeft, document.scrollingElement.scrollTop, ];
 		
 		function move( e, )
 		{
-			viewport.moveTarget( target, e.offsetX - oX, oY - e.offsetY, );
+			viewport.moveTarget( target, e.clientX - cX, cY - e.clientY, );
 			
-			[ oX, oY, ]= [ e.offsetX, e.offsetY, ];
+			[ cX, cY, ]= [ e.clientX, e.clientY, ];
+		}
+		
+		function scroll( e, )
+		{
+			viewport.moveTarget( target, document.scrollingElement.scrollLeft - sX, sY - document.scrollingElement.scrollTop, );
+			
+			[ sX, sY, ]= [ document.scrollingElement.scrollLeft, document.scrollingElement.scrollTop, ];
 		}
 		
 		function stop()
 		{
 			document.removeEventListener( 'mousemove', move, )
 			
+			document.removeEventListener( 'scroll', scroll, )
+			
 			document.removeEventListener( 'mouseup', stop, )
 		}
 		
 		document.addEventListener( 'mousemove', move, )
+		
+		document.addEventListener( 'scroll', scroll, )
 		
 		document.addEventListener( 'mouseup', stop, )
 	}, );
