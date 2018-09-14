fb9e6d42207c9e1b779b552ea712880115b3d6f6
Fenz <uukooo@126.com>
2018-09-14 21:10:19 +0800

    update route using

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 import Router from '../OvO/routing/Router.js';
-import { resolve, } from '../OvO/support/path.js';
 
-const router= new Router();
-
-router.pageDir= resolve( '../pages', );
-router.basePath= resolve( '..', );
+const router= new Router( '..', '../pages', );
 
 router.route( 'home', '/', 'index', );
 router.route( 'equilateral-triangle', '/demos/equilateral-triangle', );
 router.route( 'angular-bisector', '/demos/angular-bisector', );
 router.route( 'tangent-line-of-circle', '/demos/tangent-line-of-circle', );
+router.route( {
+	home: { path: '/', page: 'index', },
+	equilateral_triangle: '/demos/equilateral-triangle',
+	angular_bisector: '/demos/angular-bisector',
+	tangent_line_of_circle: '/demos/tangent-line-of-circle',
+}, );
 
 export default router;
